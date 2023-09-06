8fbbe8e372be2cf6e6e5e0f55636aef7a98e8a19
Fabiolaolmos <138613359+Fabiolaolmos@users.noreply.github.com>
2023-09-06 19:55:48 +0000

    Configuración de archivos para la DB en MongoDB.

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
-const devConfig = {};
-const testConfig = {};
-const prodConfig = {};
+const devConfig = { MONGO_URL: 'mongodb://localhost/makeanodejsapi-dev', }; 
+const testConfig = { MONGO_URL: 'mongodb://localhost/makeanodejsapi-test', }; 
+const prodConfig = { MONGO_URL: 'mongodb://localhost/makeanodejsapi-prod', };
 const defaultConfig = {
 PORT: process.env.PORT || 3000,
 };
@@ -18,3 +18,5 @@ function envConfig(env) {
 export default { ...defaultConfig,
      ...envConfig(process.env.NODE_ENV),
  };
+
+
