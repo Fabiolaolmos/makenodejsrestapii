73e59fcca59badf5e96bd96900d71744eb97222d
Fabiolaolmos <138613359+Fabiolaolmos@users.noreply.github.com>
2023-09-06 18:40:06 +0000

    Instación de mongoose

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import express from "express";
+import express from 'express';
+import constants from './config/constants';
 const app = express();
-const PORT = process.env.PORT || 300;
-app.listen(PORT, err => {
-if (err) {
-throw err;
-} else {
-console.log(`Server running on port: $ {
-PORT
-}-- - Running on $ {
-process.env.NODE_ENV
-}-- - Make something great!`)
-}
+app.listen(constants.PORT, err => {
+    if (err) {
+        throw err;
+    } else {
+        console.log(`Server running on port: ${constants.PORT} 
+        --- Running on ${process.env.NODE_ENV} --- Make something great.!`)
+    }
 });
